40cfbe2ab3ea034b082d8349c13901974767e6b5
Aarnav Bos <aarnavbos@gmail.com>
2017-10-06 06:24:16 -0400

    introducing mongodb

--- main.go
+++ main.go
@@ -1,15 +1,76 @@
 package main
 
 import "github.com/gin-gonic/gin"
-import "net/http"
+//import "net/http"
+import "fmt"
+import "gopkg.in/mgo.v2"
+//import "gopkg.in/mgo.v2/bson"
 
 func main() {
 	router := gin.Default()
-	router.GET("/user/:name",func(c *gin.Context) {
-		name := c.Param("name")
-		c.String(http.StatusOK,"Hello %s",name)
-		
-	})
-	router.Run()
+	session, err := mgo.Dial("mongodb://aegis:Goldroger1@ds121534.mlab.com:21534/aegis")
+	c := session.DB('aegis').C('users')
+
+	//login group
+	route := router.Group("/")
+	{
+		//login
+		route.GET("/login",func(c *gin.Context){
+			c.JSON(200,gin.H{
+				"message":"pong",
+				})
+			})
+		route.POST("/login",func(c *gin.Context){
+			//get JSON data
+			var data struct {
+				username string `json:"username" binding:"required"`
+				password string `json:"password" binding:"required"`
+				imageHash string `json:"img_hash" binding:"required"`
+			}
+			//Check if user exists
+
+			c.JSON(200,gin.H{
+				"message":"pong",
+				})
+			})
+
+		//signup
+		route.GET("/signup",func(c *gin.Context){
+			c.JSON(200,gin.H{
+				"message":"pong",
+				})
+			})
+		route.POST("/signup",func(c *gin.Context){
+			c.JSON(200,gin.H{
+				"message":"pong",
+				})
+			})
+
+
+		//view all notes
+		route.POST("/view_all_notes/",func(c *gin.Context) {
+			//collect JSON Data and store in dict
+
+			var json struct {
+				Username string `json:"username" binding:"required"`
+
+			}
+
+			//Bind
+			c.Bind(&json)
+
+			//return resposne
+			c.JSON(200,json)
+			fmt.Println("works")
+			
+
+
+			
+		})
+
+	}
+	
+	router.Run(":5000")
 	
 }
+
